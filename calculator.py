624ef70a99676c0d0dcc326fa30425ed7d8a7c65
Inbar Gil <inbar.gil.41@gmail.com>
2021-04-12 10:51:12 +0300

    removed magic numbers
    made global vars for magic numbers

--- calculator.py
+++ calculator.py
@@ -5,6 +5,10 @@ import os
 # Adding the regex pattern to find the operators
 patt = re.compile(r"(-?\d+)[ \t]*([^\w\s])[ \t]*(-?\d+)")
 PADDING = ">"
+PRETTY_TEXT_LENGTH = 40
+X_GROUP = 1
+Y_GROUP = 3
+OPERATOR_GROUP = 2
 # The following code loads all the operators dynamically, this is kind of a bad practice but I had to add it for your
 # exercise
 for module in os.listdir(os.path.join(os.path.dirname(__file__), 'operators')):
@@ -25,7 +29,7 @@ class Calculator:
         """
         This method runs the calculator, and presents the UI to the user
         """
-        print("*" * 40)
+        print("*" * PRETTY_TEXT_LENGTH)
         print("* Welcome to our super calculator")
         print("* In order to exit type exit")
         print("* The currently supported operations:")
@@ -36,7 +40,7 @@ class Calculator:
         if len(self.operators) == 0:
             print("* We don't support any operators yet")
             exit()
-        print("*" * 40)
+        print("*" * PRETTY_TEXT_LENGTH)
         while True:
             inp = input(PADDING + " ")
             if inp == "exit":
@@ -47,7 +51,7 @@ class Calculator:
                 print(PADDING, "Please follow the syntax: <number><operation><number>")
                 continue
             # We check to see if the operator we were given is supported
-            operator = group.group(2)
+            operator = group.group(OPERATOR_GROUP)
             operation = None
             for operat in self.operators:
                 if operat.get_symbol() == operator:
@@ -56,8 +60,8 @@ class Calculator:
             if operation is None:
                 print(PADDING, "Unsupported operator")
                 continue
-            x = int(group.group(1))
-            y = int(group.group(3))
+            x = int(group.group(X_GROUP))
+            y = int(group.group(Y_GROUP))
             # If  it is supported we will print out the result
             print(PADDING, operation.act(x, y))
         print("Good bye")
